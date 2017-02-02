a09ec5dad12253e6d88e92703d6ac2e6e715cc22
SR-Alchimie <Subodh.rawat@gmail.com>
2017-02-02 06:35:13 -0600

    Update ProductRepository.java

--- src/main/java/com/myretail/product/ProductRepository.java
+++ src/main/java/com/myretail/product/ProductRepository.java
@@ -9,9 +9,6 @@ public interface ProductRepository extends MongoRepository<Product, String> {
 
 	public Product findByProductId(long productId);
 
-	@Query(value = "{ $or: [ { 'productId' : ?0 }, { 'name' : ?1 } ] }")
-	public Product findByProductIdAndName(long productId, String name);
-
 	public List<Product> findByName(String name);
 
 }
